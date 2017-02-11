a357cdeee99829ff87051597b88377f8b47ef239
holma198 <holma198@morris.umn.edu>
2017-02-11 17:07:52 -0600

    Add todo to our navbar, and had it list all todos (owner)

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,13 +3,18 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { KittensComponent } from './kittens/kittens.component';
-import {UserListComponent} from "./users/user-list.component";
+import { UserListComponent } from "./users/user-list.component";
+import { TodoListComponent } from "./todo/todo-list.component";
+
 
 // Route Configuration
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'kittens', component: KittensComponent },
-    { path: 'users', component: UserListComponent }
+    { path: 'users', component: UserListComponent },
+    { path: 'todo', component: TodoListComponent }
+
 ];
+
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
